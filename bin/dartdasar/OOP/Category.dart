2292c5e8328ec6836b9cb96faa4835914d26830c
indra22-arch <jakskuy3@gmail.com>
2025-11-15 21:25:07 +0800

    sedikit perbaikan

--- bin/dartdasar/OOP/Category.dart
+++ bin/dartdasar/OOP/Category.dart
@@ -1,16 +1,6 @@
 class Category {
-  String id = "";
-  String name = "";
+  String id = "638262626427437";
+  String name = "BBBBBBBBB";
 
   Category(this.id, this.name);
-}
-
-void main() {
-  var category1 = Category("1", "Pulpen");
-  var category2 = Category("2", "Buku");
-
-  print(category1.id);   // 1
-  print(category1.name); // Electronics
-  print(category2.id);   // 2
-  print(category2.name); // Books
 }
